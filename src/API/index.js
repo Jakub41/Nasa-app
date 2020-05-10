07f95fa8d438aaa5aa7096ad0685c9bbcda59c40
Jakub <lemiszewski@gmx.com>
2020-05-10 23:49:38 +0200

    Added Wether Mars API URL

--- src/API/index.js
+++ src/API/index.js
@@ -1,9 +1,15 @@
 import { NasaApiQuery } from './NasaApiQuery';
-import { POD_URL } from '../Config';
+import { POD_URL, W_MARS_URL } from '../Config';
 
-// GET Picture of the day 
+// GET Picture of the day
 export const getPod = NasaApiQuery(() => {
     return `${POD_URL}`;
 });
 
+// GET Mars Weather
+export const getWeatherMars = NasaApiQuery(() => {
+  return `${W_MARS_URL}`;
+});
+
 console.log("POD >> ", getPod());
+console.log("Mars Weather >> ", getWeatherMars());
--- src/Config/index.js
+++ src/Config/index.js
@@ -1,5 +1,6 @@
 /* Config of the App */
 
-// NASA API 
+// NASA API
 export const API_KEY = process.env.REACT_APP_API_KEY
 export const POD_URL = process.env.REACT_APP_POD_URL
+export const W_MARS_URL = process.env.REACT_APP_WEATHER_MARS
