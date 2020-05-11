177f290f0f2e64be3578abc26c69a7a22e066089
Jakub <lemiszewski@gmx.com>
2020-05-11 12:04:42 +0200

    Starting the Mars weather part

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      wMarsData: [],
+      isFetching: false,
+      isLoading: true,
+      error: null,
+    };
+  }
+
   render() {
     return (
       <>
-        <h1>HOME</h1>
+
       </>
     )
   }
