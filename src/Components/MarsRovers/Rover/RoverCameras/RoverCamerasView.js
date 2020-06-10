42640e389b02d66369f2711fadf8096be0491111
Jakub <lemiszewski@gmx.com>
2020-06-10 16:02:36 +0200

    Added base view list of the cameras of a Rover

--- src/Components/MarsRovers/Rover/RoverCameras/RoverCamerasView.js
+++ src/Components/MarsRovers/Rover/RoverCameras/RoverCamerasView.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-const RoverCamerasView = () => {
-  // const roverCameras = {
-  //   curiosity: ['FHAZ', 'RHAZ', 'MAST', 'CHEMCAM', 'MAHLI', 'MARDI', 'NAVCAM'],
-  //   opportunity: ['FHAZ', 'RHAZ', 'PANCAM', 'MINITES', 'NAVCAM'],
-  //   spirit: ['FHAZ', 'RHAZ', 'PANCAM', 'MINITES', 'NAVCAM'],
-  // };
-  return <h1>hello</h1>;
+const RoverCamerasView = ({ changeCamera, roverCameras }) => {
+  return (
+    <>
+      <h2>Cameras:</h2>
+      {roverCameras.map((roverCamera) => {
+        return (
+          <button type="submit" key={uuidv4()} onClick={() => changeCamera(roverCamera)}>
+            {roverCamera}
+          </button>
+        );
+      })}
+    </>
+  );
 };
 
 export default RoverCamerasView;
--- src/Components/MarsRovers/Rover/RoverCameras/index.js
+++ src/Components/MarsRovers/Rover/RoverCameras/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RoverCamerasView from './RoverCamerasView';
+import RoverCamerasConst from './RoverCamerasConst';
 
 export default function RoverCamerasIndex({ roverName }) {
+  const initialCamera = RoverCamerasConst[roverName.toLowerCase()][0];
+  const [camera, setCamera] = useState(initialCamera);
+
   return (
     <div>
-      <RoverCamerasView roverName={roverName} />
+      <h1>Photos taken by {roverName}</h1>
+      <hr />
+      <RoverCamerasView
+        camera={camera}
+        changeCamera={setCamera}
+        roverCameras={RoverCamerasConst[roverName.toLowerCase()]}
+        roverName={roverName}
+      />
     </div>
   );
 }
